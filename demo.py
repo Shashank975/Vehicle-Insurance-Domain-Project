aff119bc57028e081503dc1ba959a6d91f4fe12b
Shashank Chhoker <shashanksaad07@gmail.com>
2025-05-25 03:48:57 +0530

    data ingestion done

--- demo.py
+++ demo.py
@@ -1,12 +1,18 @@
 # from src.logger import logging
 
 # logging.debug("This is for the test2")
-import sys
-from src.logger import logging
-from src.exception import MyException  # ✅ Correct import
 
-try:
-    1 / 0
-except Exception as e:
-    raise MyException(str(e), sys)
+#------------------------------------------------------
+# import sys
+# from src.logger import logging
+# from src.exception import MyException  # ✅ Correct import
 
+# try:
+#     1 / 0
+# except Exception as e:
+#     raise MyException(str(e), sys)
+#------------------------------------------------------
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
